5ab30e859305f3e49f6f773e8e6f808861b1aa3b
Xthani <muncoval@gmail.com>
2023-11-02 20:53:24 +0600

    lesson 3 useEffect useState

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import { useState } from "react";
 import styles from "./App.module.css";
 import AddTask from "./components/AddTask/AddTask";
 import List from "./components/List/List";
+import Count from "./components/Count/Count";
 
 function App() {
   const [count, setCount] = useState(1);
-  console.log(count, 'count');
+  // console.log(count, 'count');
 
   const arr = ['item 1', 'item 2', 'item 3', 'item 4'];
   const [a, b, ...c] = arr;
@@ -22,6 +23,7 @@ function App() {
   return (
     <>
       <h1 className={styles.list}>TODO LIST</h1>
+      <Count/>
       <button onClick={handleCount} >CLICK</button>
       <AddTask/>
       <List
